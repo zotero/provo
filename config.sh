633228eb44424b76e80744126a0b38d6e2d216ad
Simon Kornblith <simon@simonster.com>
2012-03-02 15:47:34 -0500

    Fix OS detection

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ OUTPUT_DIRECTORY="$SCRIPT_DIRECTORY/output/`date -u +%Y-%m-%d`"
 
 # Safari extension directory
 # Safari homepage must be set to http://127.0.0.1:23119/provo/run for testing
-if [ "`uname`" != "Darwin" ]; then
+if [ "`uname`" == "Darwin" ]; then
 	SAFARI_CACHE_DIRECTORY="$HOME/Library/Caches/com.apple.Safari"
 	SAFARI_EXTENSION_LOCATION="$HOME/Library/Caches/Safari/Extensions/Zotero Connector for Safari.safariextz"
 elif [ "`uname -o 2> /dev/null`" == "Cygwin" ]; then
