f923f308b14f46aa0e0a514cb28e71bf1aeb2fef
Simon Kornblith <simon@simonster.com>
2013-04-14 15:54:27 -0400

    Fuck sed

--- test.sh
+++ test.sh
@@ -242,9 +242,8 @@ function testBranch {
 	git checkout "$BRANCH"
 	git pull
 	SUFFIX="$BRANCH.SOURCE.`git log -n 1 --pretty='format:%h'`"
-	sed -i 'bak' 's/https:\/\/www\.zotero\.org\/bookmarklet\//http:\/\/127.0.0.1:23119\/provo\/bookmarklet\//g' \
+	perl -pi -e 's/https:\/\/www\.zotero\.org\/bookmarklet\//http:\/\/127.0.0.1:23119\/provo\/bookmarklet\//g' \
 		chrome/content/zotero/xpcom/zotero.js
-	rm chrome/content/zotero/xpcom/zotero.js.bak
 	popd
 	
 	# Build connectors
