3ee657e639ac44f2fb81aeedbc6972709ea03d0c
Dan Stillman <dstillman@zotero.org>
2018-12-14 00:53:48 -0500

    Use HTTP rather than Git for repo clones
    So that this can run without needing SSH credentials to be set up

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-STANDALONE_BUILD_REPO="git://github.com/zotero/zotero-standalone-build.git"
+STANDALONE_BUILD_REPO="https://github.com/zotero/zotero-standalone-build.git"
 STANDALONE_BUILD_DIR="$SCRIPT_DIR/zotero-standalone-build"
 
-ZOTERO_BUILD_REPO="git://github.com/zotero/zotero-build.git"
+ZOTERO_BUILD_REPO="https://github.com/zotero/zotero-build.git"
 ZOTERO_BUILD_DIR="$SCRIPT_DIR/zotero-build"
 
-CONNECTORS_REPO="git://github.com/zotero/zotero-connectors.git"
+CONNECTORS_REPO="https://github.com/zotero/zotero-connectors.git"
 CONNECTORS_DIR="$SCRIPT_DIR/zotero-connectors"
 
-TRANSLATORS_REPO="git://github.com/zotero/translators.git"
+TRANSLATORS_REPO="https://github.com/zotero/translators.git"
 TRANSLATORS_DIR="$SCRIPT_DIR/translators"
 
-TRANSLATION_SERVER_REPO="git@github.com:zotero/translation-server.git"
+TRANSLATION_SERVER_REPO="https://github.com/zotero/translation-server.git"
 TRANSLATION_SERVER_DIR="$SCRIPT_DIR/translation-server"
 
 TEMP_PROFILE_DIR="$SCRIPT_DIR/tmp_profile"
